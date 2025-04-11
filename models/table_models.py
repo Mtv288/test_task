9c7c9321802ba6dbef96626a2c22022cac08970e
Mtv288 <meremkulovtv@gmail.com>
2025-04-11 22:15:36 +0300

    table

--- models/table_models.py
+++ models/table_models.py
@@ -3,8 +3,6 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
 
 
-
-
 class BASE(DeclarativeBase):
     pass
 
@@ -18,7 +16,10 @@ class Table(BASE):
     seats: Mapped[int]
     location: Mapped[str]
 
-    reservation: Mapped["Reservation"] = relationship("Reservation", back_populates="table")
+    reservation: Mapped["Reservation"] = relationship(
+        "Reservation", back_populates="table",
+        cascade="all, delete-orphan"
+    )
 
     def __repr__(self):
         return f'{self.name}, {self.seats}, {self.location}'
@@ -34,7 +35,10 @@ class Reservation(BASE):
     reservation_time: Mapped[datetime]
     duration_minutes: Mapped[int]
 
-    table: Mapped["Table"] = relationship("Table", back_populates="reservation")
+    table: Mapped["Table"] = relationship(
+        "Table", back_populates="reservation",
+        cascade="all, delete-orphan"
+    )
 
     def __repr__(self):
         return f'{self.customer}. {self.table_id}, {self.reservation_time}, {self.duration_minutes}'
